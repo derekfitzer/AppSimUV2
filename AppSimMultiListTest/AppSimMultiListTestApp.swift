48cc55a4c41d24f1e402f4b14782cbd8e3306d1a
Derek Fitzer <franzderek@gmail.com>
2025-05-05 10:39:43 -0400

    Initial Commit

---
+++ AppSimMultiListTest/AppSimMultiListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppSimMultiListTestApp.swift
+//  AppSimMultiListTest
+//
+//  Created by Derek Fitzer on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AppSimMultiListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
