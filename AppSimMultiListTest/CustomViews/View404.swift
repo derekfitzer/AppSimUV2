ba83ccde8d276555c8a29e07011c6cb6777df1c3
Derek Fitzer <franzderek@gmail.com>
2025-05-14 09:14:32 -0400

    added student locations

---
+++ AppSimMultiListTest/CustomViews/View404.swift
@@ -0,0 +1,18 @@
+//
+//  View404.swift
+//  AppSimMultiListTest
+//
+//  Created by Derek Fitzer on 5/13/25.
+//
+
+import SwiftUI
+
+struct View404: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View404()
+}
